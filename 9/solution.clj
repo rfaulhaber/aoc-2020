fb89ea8dc8dede8f9b44d0a7bd97e1e1fa45eb14
Ryan Faulhaber <ryf@sent.as>
2020-12-26 14:16:40 -0500

    Day 9 part 2

--- 9/solution.clj
+++ 9/solution.clj
@@ -15,6 +15,22 @@
                 [(first coll) y])
               (pairs s))))
 
+(defn sum [l]
+  (reduce + l))
+
+; seems inefficient, can this be improved?
+(defn find-run [target input]
+  (loop [n 2
+         run (take 2 input)]
+    (cond
+      (= target (sum run)) run
+      (> (sum run) target) (find-run target (next input))
+      :else (recur (inc n) (take (inc n) input)))))
+
+(defn xmas-valid? [preamble val]
+  (let [p (set (map (partial apply +) (pairs preamble)))]
+    (contains? p val)))
+
 (defn part-1 [preamble-len input]
   (let [[preamble rs] (split-at preamble-len input)
         val (first rs)]
@@ -22,23 +38,14 @@
       val
       (part-1 preamble-len (next input)))))
 
-;; (defn part-2 [target input]
-;;   (if (> target (first input))
-;;     (part-2 target (next input))
-;;     (loop [sum 0
-;;            idx 0]
-;;       (let [val (first input)]
-;;       (if (> (+ sum val) target)
-;;         (part-2 target (second (split-at idx input)))
-;;         (recur )
-;;         )
-;;         )
-;;       )
-;;     )
-;;   )
+(defn part-2 [target input]
+  (let [run (find-run target input)
+        min (apply min run)
+        max (apply max run)]
+    (+ min max)))
 
-(defn xmas-valid? [preamble val]
-  (let [p (set (map (partial apply +) (pairs preamble)))]
-    (contains? p val)))
+(def parsed-input (parse-input input))
+(def target (part-1 25 parsed-input))
 
-(println "Part 1: " (part-1 25 (parse-input input)))
+(printf "Part 1: %d\n" target)
+(printf "Part 2: %d\n" (part-2 target parsed-input))
